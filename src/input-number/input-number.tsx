006b88084bd5c181a5d10f438dfeb2f0f02f6d19
Etty <beltrami.ester@gmail.com>
2023-01-18 19:54:05 +0000

    Fix background

--- src/input-number/input-number.tsx
+++ src/input-number/input-number.tsx
@@ -4,21 +4,20 @@ import { Text } from "../text";
 import React from "react";
 
 type Props = {
-  values: { value: number | string ; label: string | React.ReactNode }[];
+  values: { value: number | string; label: string | React.ReactNode }[];
   value?: number | string | undefined;
   onClick?: (value: number | string) => void;
 };
 
 export const InputNumber = ({ values, value, onClick }: Props) => {
-  
   const valuesById = Object.fromEntries(
     values.map((value) => [value.value, value.label])
   );
 
-  const handleOnClick = (newValue: number | string): () => void => {
+  const handleOnClick = (newValue: number | string): (() => void) => {
     return () => {
       onClick && onClick(newValue);
-    }
+    };
   };
 
   return (
@@ -29,8 +28,10 @@ export const InputNumber = ({ values, value, onClick }: Props) => {
             key={element.value}
             className={clsx(
               "flex items-center justify-center font-sans text-md  rounded-full border-3  hover:bg-green cursor-pointer border-black w-10 h-10 md:w-16 md:h-16  transition-colors outline-none  leading-3 ",
-              { "bg-green": value === element.value,
-            "bg-white": value !== element.value }
+              {
+                "bg-green": value === element.value,
+                "bg-white": value !== element.value,
+              }
             )}
             onClick={handleOnClick(element.value)}
           >
@@ -39,9 +40,7 @@ export const InputNumber = ({ values, value, onClick }: Props) => {
         ))}
         <div className="flex h-10 md:h-16">
           <div className="flex items-center justify-center">
-            <Text size="label2" >
-              {value && valuesById[value]}
-            </Text>
+            <Text size="label2">{value && valuesById[value]}</Text>
           </div>
         </div>
       </div>
