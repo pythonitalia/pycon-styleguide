e5f46b8c53fcd8544300f12e713668af6b95b425
Etty <beltrami.ester@gmail.com>
2023-01-22 16:25:37 +0000

    Show text when on hover

--- src/input-number/input-number.tsx
+++ src/input-number/input-number.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { Heading } from "../heading";
 import { Text } from "../text";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
   values: { value: number | string; label: string | React.ReactNode }[];
@@ -14,9 +14,23 @@ export const InputNumber = ({ values, value, onClick }: Props) => {
     values.map((value) => [value.value, value.label])
   );
 
+  const [display, setDisplay] = useState<number | string | null>(null);
+
   const handleOnClick = (newValue: number | string): (() => void) => {
     return () => {
       onClick && onClick(newValue);
+    };
+  };
+
+  const handleOnMouseEnter = (newValue: number | string): (() => void) => {
+    return () => {
+      setDisplay(newValue);
+    };
+  };
+
+  const handleOnMouseLeave = (newValue: number | string): (() => void) => {
+    return () => {
+      setDisplay(null);
     };
   };
 
@@ -34,15 +48,25 @@ export const InputNumber = ({ values, value, onClick }: Props) => {
               }
             )}
             onClick={handleOnClick(element.value)}
+            onMouseOver={handleOnMouseEnter(element.value)}
+            onMouseLeave={handleOnMouseLeave(element.value)}
           >
             <Heading size={3}>{element.value}</Heading>
           </div>
         ))}
-        <div className="flex h-10 md:h-16">
-          <div className="flex items-center justify-center">
-            <Text size="label2">{value && valuesById[value]}</Text>
+        {values.map((element) => (
+          <div
+            className={clsx("flex h-10 md:h-16", {
+              hidden:
+                (display !== element.value && value !== element.value) ||
+                (value === element.value && display !== null),
+            })}
+          >
+            <div className="flex items-center justify-center">
+              <Text size="label2">{valuesById[element.value]}</Text>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
