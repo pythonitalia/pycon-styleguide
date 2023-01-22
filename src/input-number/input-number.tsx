b2a60864bd96c5f1a99b249bf795f71d2e3012a1
Etty <beltrami.ester@gmail.com>
2023-01-22 19:06:37 +0000

    Use css

--- src/input-number/input-number.tsx
+++ src/input-number/input-number.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { Heading } from "../heading";
 import { Text } from "../text";
-import React, { useState } from "react";
+import React from "react";
 
 type Props = {
   values: { value: number | string; label: string | React.ReactNode }[];
@@ -14,59 +14,40 @@ export const InputNumber = ({ values, value, onClick }: Props) => {
     values.map((value) => [value.value, value.label])
   );
 
-  const [display, setDisplay] = useState<number | string | null>(null);
-
-  const handleOnClick = (newValue: number | string): (() => void) => {
-    return () => {
-      onClick && onClick(newValue);
-    };
-  };
-
-  const handleOnMouseEnter = (newValue: number | string): (() => void) => {
-    return () => {
-      setDisplay(newValue);
-    };
-  };
-
-  const handleOnMouseLeave = (newValue: number | string): (() => void) => {
-    return () => {
-      setDisplay(null);
-    };
-  };
-
   return (
-    <div>
-      <div className="flex flex-row gap-1 md:gap-6">
+    <div className="relative inline-block input-number-wrapper">
+      <div className=" flex flex-row gap-1 md:gap-6">
         {values.map((element) => (
           <div
             key={element.value}
             className={clsx(
               "flex items-center justify-center font-sans text-md  rounded-full border-3  hover:bg-green cursor-pointer border-black w-10 h-10 md:w-16 md:h-16  transition-colors outline-none  leading-3 ",
+              "input-number",
               {
                 "bg-green": value === element.value,
                 "bg-white": value !== element.value,
               }
             )}
-            onClick={handleOnClick(element.value)}
-            onMouseOver={handleOnMouseEnter(element.value)}
-            onMouseLeave={handleOnMouseLeave(element.value)}
+            onClick={() => onClick?.(element.value)}
           >
             <Heading size={3}>{element.value}</Heading>
-          </div>
-        ))}
-        {values.map((element) => (
-          <div
-            className={clsx("flex h-10 md:h-16", {
-              hidden:
-                (display !== element.value && value !== element.value) ||
-                (value === element.value && display !== null),
-            })}
-          >
-            <div className="flex items-center justify-center">
+
+            <div
+              className={clsx(
+                "absolute input-number-label -bottom-2 md:bottom-auto left-0  md:left-full w-full md:ml-8 ",
+                {
+                  "active-element block": element.value === value,
+                  hidden: element.value !== value,
+                }
+              )}
+            >
               <Text size="label2">{valuesById[element.value]}</Text>
             </div>
           </div>
         ))}
+      </div>
+      <div className="md:hidden invisible">
+        <Text size="label2">Value</Text>
       </div>
     </div>
   );
